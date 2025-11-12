e56c489d01739bb24abad7a902b8027f88e2c628
GrPugaguga <pugaguga@mail.ru>
2025-11-12 19:37:54 +0500

    add pipes

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail,MinLength} from 'class-validator';
+
 export class LoginDto {
+  @IsEmail({}, { message: 'Некорректный email' })
   email: string;
+
+  @MinLength(8, { message: 'Пароль должен содержать не менее 8 символов' })
   password: string;
 }
